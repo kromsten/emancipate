d4e2cdb39ff596d6fffd93828e454220f217af89
elbok <kromsten@pm.me>
2023-03-07 18:37:52 +0100

    init project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { counter } from "$lib/stores";
+  	import Increment from "./increment.svelte";
+</script>
+
+
+
+<div class="container mx-auto p-6">
+	<h1 class="text-4xl font-bold">Hello World</h1>
+
+	<h5>Counter {$counter}</h5>
+
+	<Increment />
+</div>
---
+++ src/routes/increment.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import { counter } from "$lib/stores";
+</script>
+
+<button class="button" on:click={() => $counter++}>Increment</button>
