74384531885fd35907840b8ee87ac19cad96a04d
elbok <kromsten@pm.me>
2023-03-07 20:00:19 +0100

    connection requirement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,27 @@
 	import Tabs from '$lib/components/tabs.svelte';
 	import Artist from '$lib/components/artist.svelte';
 	import Consumer from '$lib/components/consumer.svelte';
-	import { tabSet } from '$lib/stores';
+	import { connected, tabSet } from '$lib/stores';
+	import { walletStore } from '@svelte-on-solana/wallet-adapter-core';
 </script>
 
 
 
 <div class="container mx-auto p-6">
-	<Tabs />
 
-	{ #if $tabSet === 0 }
-		<Artist />
-	{ :else if $tabSet === 1 }
-		<Consumer />
-	{/if}
+	{ #if $walletStore.connected }
+		<Tabs />
+
+		{ #if $tabSet === 0 }
+			<Artist />
+		{ :else if $tabSet === 1 }
+			<Consumer />
+		{/if}
+
+	{ :else }
+		Not connected
+	{ /if }
+
 
 </div>
 
